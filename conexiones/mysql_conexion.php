1a8b22d8e259f513276a1482219ccce41cadbf6e
Nelson martinez <nelson34@live.com.mx>
2022-06-19 18:05:03 -0500

    cambiops para php

--- conexiones/mysql_conexion.php
+++ conexiones/mysql_conexion.php
@@ -1,16 +1,17 @@
 <?php
-function    getConnection(){
-$conn=NULL;
-try {
-    define('DB_SERVER', 'localhost:3306');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_DATABASE', 'Telebachillerato');
+function getConnection()
+{
+    $conn = NULL;
+    try {
+        define('DB_SERVER', 'localhost:3306');
+        define('DB_USERNAME', 'root');
+        define('DB_PASSWORD', '');
+        define('DB_DATABASE', 'Telebachillerato');
 
-    $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-return $conn;
-} catch (Exception $e) {
-    echo 'Excepción capturada: ',  $e->getMessage(), "\n";
-}
+        $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+        return $conn;
+    } catch (Exception $e) {
+        echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+    }
 }
 ?>
--- controllers/loginController.php
+++ controllers/loginController.php
@@ -2,9 +2,9 @@
 include '../services/loginService.php';
 session_start();
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $IdUsuario=$_POST['IdUsuario'];
-    $Password=$_POST['Password'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $IdUsuario = $_POST['IdUsuario'];
+    $Password = $_POST['Password'];
     echo $IdUsuario;
     echo $Password;
     login($IdUsuario, $Password);
