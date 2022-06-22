a818a8c491158f2991a8a969681edaa960a9c293
Reyna Albiter <reyna160902@gmail.com>
2022-06-21 23:02:33 -0500

    Cambios para la captura del Alumno

--- alumnos/ListaPrimero.php
+++ alumnos/ListaPrimero.php
@@ -9,50 +9,66 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </head>
 <body>
-  <div ng-include="'../menu/menu.php'"></div>
+  
     <div class="container-fluid" style="width: 50%;">
     <form class="row g-3">
+    <center>
+        <div><h2>Datos Personales</h2></div>
+        </center>
         <div class="col-md-6">
-          <label for="inputEmail4" class="form-label">Email</label>
+          <label for="inputEmail4" class="form-label">Nombre</label>
           <input type="email" class="form-control" id="inputEmail4">
         </div>
         <div class="col-md-6">
-          <label for="inputPassword4" class="form-label">Password</label>
+          <label for="inputPassword4" class="form-label">Apelliodo Paterno</label>
           <input type="password" class="form-control" id="inputPassword4">
         </div>
-        <div class="col-12">
-          <label for="inputAddress" class="form-label">Address</label>
+        <div class="col-md-6">
+          <label for="inputAddress" class="form-label">Apellido Materno</label>
           <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
         </div>
-        <div class="col-12">
-          <label for="inputAddress2" class="form-label">Address 2</label>
+        <div class="col-md-6">
+          <label for="inputAddress2" class="form-label">Correo Electrónico</label>
           <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
         </div>
         <div class="col-md-6">
-          <label for="inputCity" class="form-label">City</label>
+          <div class="form-check">
+          <label class="form-check-label" for="gridCheck">
+              Activo
+            </label>
+            <input class="form-check-input" type="checkbox" id="gridCheck">
+          </div>
+        </div>
+        
+        <hr class="border-secondary border-3 opacity-75">
+
+
+        <center>
+        <div><h2>Datos Escolares</h2></div>
+        </center>
+        <div class="col-md-6">
+          <label for="inputCity" class="form-label">Generación</label>
           <input type="text" class="form-control" id="inputCity">
         </div>
-        <div class="col-md-4">
-          <label for="inputState" class="form-label">State</label>
+        <div class="col-md-6">
+          <label for="inputState" class="form-label">Grado</label>
           <select id="inputState" class="form-select">
             <option selected>Choose...</option>
             <option>...</option>
           </select>
         </div>
-        <div class="col-md-2">
-          <label for="inputZip" class="form-label">Zip</label>
+        <div class="col-md-6">
+          <label for="inputZip" class="form-label">Año</label>
           <input type="text" class="form-control" id="inputZip">
         </div>
-        <div class="col-12">
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="gridCheck">
-            <label class="form-check-label" for="gridCheck">
-              Check me out
-            </label>
-          </div>
+        <div class="col-md-6">
+          <label for="inputCity" class="form-label">No.Lista</label>
+          <input type="text" class="form-control" id="inputCity">
         </div>
+
+        
         <div class="col-12">
-          <button type="submit" class="btn btn-primary">Sign in</button>
+          <button type="submit" class="btn btn-primary">Guardar</button>
         </div>
       </form>
     </div>
